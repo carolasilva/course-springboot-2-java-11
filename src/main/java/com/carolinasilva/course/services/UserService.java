28040509d98a3607d64654d6a35f97c2f447b03c
Carolina Alves <carol.alves99@live.com>
2019-12-26 08:49:10 -0300

    Exception handling - delete

--- src/main/java/com/carolinasilva/course/services/UserService.java
+++ src/main/java/com/carolinasilva/course/services/UserService.java
@@ -2,8 +2,11 @@ package com.carolinasilva.course.services;
 
 import com.carolinasilva.course.entities.User;
 import com.carolinasilva.course.repositories.UserRepository;
+import com.carolinasilva.course.services.exceptions.DatabaseException;
 import com.carolinasilva.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -28,7 +31,13 @@ public class UserService {
   }
 
   public void delete(Long id) {
-    userRepository.deleteById(id);
+    try {
+      userRepository.deleteById(id);
+    } catch (EmptyResultDataAccessException e) {
+      throw new ResourceNotFoundException(id);
+    } catch (DataIntegrityViolationException e) {
+      throw new DatabaseException(e.getMessage());
+    }
   }
 
   public User update(Long id, User user) {
